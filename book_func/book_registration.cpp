35f79a31bb2efc2209938e91ee76f0e5687f709f
IKAROS93 <386188405@qq.com>
2022-01-13 10:49:11 +0800

    将addinfo改名

---
+++ book_func/book_registration.cpp
@@ -0,0 +1,33 @@
+#include "head.h"
+
+/* 实现书籍注册以及借还功能 */
+
+// 添加书籍信息
+void addbook()
+{
+
+}
+
+// 借出人信息
+void lender_info()
+{
+
+}
+
+// 借书登记
+void BookLender()
+{
+
+}
+
+// 还书登记
+void BookComeback()
+{
+
+}
+
+// 删除书籍
+void DeleteBook()
+{
+
+}
