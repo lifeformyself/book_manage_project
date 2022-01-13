6f18865da96c965d6b942f179f507298b99067fa
IKAROS93 <386188405@qq.com>
2022-01-13 10:50:01 +0800

    修正显示bug（utf-8），添加函数实现框架

--- HEAD/head.h
+++ HEAD/head.h
@@ -4,18 +4,19 @@
 
 #include <iostream>
 #include <string>
+#include <unistd.h>//ubuntu下保留控制台所需头文件
 
 
 
 using namespace std;
 
 void showMenu();    //显示菜单
-void select();       //功能选择
+void select();      //功能选择
 void addbook();     //添加图书
-void lender_info();   //借出人信息
+void lender_info(); //借出人信息
 void BookLender();  //借书登记
 void BookComeback();//还书登记
-void DeleteBook(); //删除书籍
+void DeleteBook();  //删除书籍
 
 //书籍结构体
 struct book_info
@@ -38,7 +39,7 @@ struct BookManage
 {
     struct book_info bookArray[MAX];    //保存的书籍数组
     int m_number;                       //管理系统中书籍数量
-}；
+};
 
 
 #endif
