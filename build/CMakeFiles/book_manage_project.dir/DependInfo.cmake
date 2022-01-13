a69fc2491e5e7e9f48b0957088a574c730e5746b
IKAROS93 <386188405@qq.com>
2022-01-13 10:51:26 +0800

    编译相关

---
+++ build/CMakeFiles/book_manage_project.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ikaros/book_manage_project/book_func/book_registration.cpp" "/home/ikaros/book_manage_project/build/CMakeFiles/book_manage_project.dir/book_func/book_registration.cpp.o"
+  "/home/ikaros/book_manage_project/book_func/select.cpp" "/home/ikaros/book_manage_project/build/CMakeFiles/book_manage_project.dir/book_func/select.cpp.o"
+  "/home/ikaros/book_manage_project/book_func/showMenu.cpp" "/home/ikaros/book_manage_project/build/CMakeFiles/book_manage_project.dir/book_func/showMenu.cpp.o"
+  "/home/ikaros/book_manage_project/main.cpp" "/home/ikaros/book_manage_project/build/CMakeFiles/book_manage_project.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../book_func"
+  "../HEAD"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/book_manage_project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/book_manage_project.dir/book_func/book_registration.cpp.o"
+  "CMakeFiles/book_manage_project.dir/book_func/select.cpp.o"
+  "CMakeFiles/book_manage_project.dir/book_func/showMenu.cpp.o"
+  "CMakeFiles/book_manage_project.dir/main.cpp.o"
+  "book_manage_project"
+  "book_manage_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/book_manage_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
