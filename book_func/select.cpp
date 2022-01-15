888d4495f802f174afa4e06987a9cdd5f4e921bf
unknown <742885206@qq.com>
2022-01-15 23:55:43 +0800

    修改按任意键无法返回主菜单bug

--- book_func/select.cpp
+++ book_func/select.cpp
@@ -4,6 +4,7 @@ void select()
 {
 	while (1)
 	{
+		showMenu();
 		cout << "请选择对应功能的标号" << endl;
 		int i = 0;
 		cin >> i;
@@ -12,24 +13,36 @@ void select()
 		case 1:
 			addbook();
 			break;
+			system("cls");
 		case 2:
 			BookLender();
 			break;
+			system("cls");
 		case 3:
 			BookComeback();
 			break;
+			system("cls");
 		case 4:
 			DeleteBook();
 			break;
+			system("cls");
 		case 0:
 			cout << "欢迎下次使用" << endl;
 			pause();//ubuntu下保留控制台指令
 			return;
+		default:
+			cout << "无效的指令" << endl;
+			break;
+			system("cls");
 		}
 	}
 }
 
 /*  存在bug
 	按任意建应该返回菜单目录
+
+
 	进入子项功能后，设计一个保存键，保存并返回上级菜单；设计一个返回键，提示不保存当前操作，不保存用户输入并返回上级菜单
+	此功能应在具体函数中实现
+	
 	按0才是退出 */
